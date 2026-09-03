4501d444eb3a89ed3747148f97b2ded49f05a1ee
carlitroxxx <ca.moil@duocuc.cl>
2026-09-03 15:26:09 -0400

    Version 1.0.0: Inicio de microservicio carrito

---
+++ src/main/java/com/snkrlab/carrito/SnkrlabMsCarritoApplication.java
@@ -0,0 +1,13 @@
+package com.snkrlab.carrito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnkrlabMsCarritoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnkrlabMsCarritoApplication.class, args);
+	}
+
+}
