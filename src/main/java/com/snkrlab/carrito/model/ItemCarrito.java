683277b6ad69c77449ea7a7f4306e6ee88e6a7c9
carlitroxxx <ca.moil@duocuc.cl>
2026-09-05 02:22:27 -0400

    Version 1.3.2:(fix) Arreglo model

--- src/main/java/com/snkrlab/carrito/model/ItemCarrito.java
+++ src/main/java/com/snkrlab/carrito/model/ItemCarrito.java
@@ -12,7 +12,7 @@ import lombok.*;
 public class ItemCarrito {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Long id;
     @Column(nullable = false)
     private String usuarioId;
     @Column(nullable = false)
