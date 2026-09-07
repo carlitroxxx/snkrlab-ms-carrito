d6cd6350bf5e335b3556e74142ee150b2810143b
carlitroxxx <ca.moil@duocuc.cl>
2026-09-07 04:23:59 -0300

    Version 1.7.1: fix PATCH no soportado por feign

--- src/main/java/com/snkrlab/carrito/client/ProductoClient.java
+++ src/main/java/com/snkrlab/carrito/client/ProductoClient.java
@@ -3,10 +3,7 @@ package com.snkrlab.carrito.client;
 import com.snkrlab.carrito.config.FeignClientConfig;
 import com.snkrlab.carrito.dto.ProductoDTO;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient(
         name = "ms-productos",
@@ -18,6 +15,6 @@ public interface ProductoClient {
     @GetMapping("/api/v1/productos/{id}")
     ProductoDTO obtenerProductoPorId(@PathVariable("id") Long id);
 
-    @PatchMapping("/api/v1/productos/{id}/stock")
+    @PutMapping("/api/v1/productos/{id}/stock")
     ProductoDTO reducirStock(@PathVariable("id") Long id, @RequestParam("cantidad") Integer cantidad);
 }
